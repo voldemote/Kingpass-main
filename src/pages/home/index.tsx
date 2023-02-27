95f353d4d5867f036c210dfff57da880e2b2019c
webexpert821 <jollydev0821@gmail.com>
2023-02-28 01:26:04 +0900

    Extend card is completed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,8 @@ export const Home = () => {
     <>
       <KingpassDetail />
       <BenefitsEra />
-      {isConnected && <KingpassClaim />}
+      {/* {isConnected && <KingpassClaim />} */}
+      <KingpassClaim />
       <FaqSection />
     </>
   );
